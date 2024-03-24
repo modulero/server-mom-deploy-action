3cbea90db14634b53ed73907059975e4883262c9
Richard Hansma <richard_hansma@hotmail.com>
2024-03-24 17:04:06 +0100

    Setup Server Mom Deploy Action

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const domain = 'server-mom.modulero.nl';
+
+    const account = core.getInput('account');
+    const project = core.getInput('project');
+    const email = core.getInput('email');
+    const apiKey = core.getInput('api_key');
+    const serverId = core.getInput('server_id');
+    const branch = core.getInput('branch');
+    const revision = core.getInput('revision');
+
+    fetch(`https://${account}.${domain}/projects/${project}/deployments`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + btoa(email + ':' + apiKey),
+        },
+        body: JSON.stringify({
+            server_id: serverId,
+            branch: branch,
+            revision: revision,
+        })
+    })
+        .then(response => response.json())
+        .then(json => core.setOutput('output', json));
+} catch (error) {
+    core.setFailed(error.message);
+}
